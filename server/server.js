2aa13e2b91016b5ab2eb3b814e5b4d75d1e870cd
Hayato <olijnikm132@gmail.com>
2026-01-20 03:22:22 +0200

    refactoring code

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 require("./src/config/env");
 const http = require("http");
-const ws = require("./src/websocket/ws");
+const ws = require("./src/utils/websocket/ws");
 
 const app = express();
 
