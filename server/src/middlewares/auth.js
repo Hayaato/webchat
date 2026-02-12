7b9102c462ec4ee0b4a5129e79405faabe10cf54
Hayato <olijnikm132@gmail.com>
2026-02-12 03:50:30 +0200

    auth blacklist.js checking added

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
-
+const set = require('../utils/blacklist')
 function auth(req, res, next){
     try{
         const header = req.headers.authorization;
         if (!header) return res.sendStatus(401);
-
         const token = header.split(" ")[1];
         const decoded = jwt.verify(token, secret);
         req.user = decoded.login;
+        if(set.Blacklist.has(req.user))
         next();
     }
     catch{
--- server/src/utils/blacklist.js
+++ server/src/utils/blacklist.js
@@ -1,4 +1,3 @@
 let Blacklist = new Set;
-let Jwt_Blacklist = new Set;
 
-module.exports = {Blacklist, Jwt_Blacklist};
+module.exports = {Blacklist};
